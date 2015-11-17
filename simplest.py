04a53055ea661370650699d886a339fd010576b5
Michael Brown <mbrown@pacificbiosciences.com>
2015-11-17 13:18:14 -0800

    simplest

---
+++ simplest.py
@@ -0,0 +1,18 @@
+import h5py
+import numpy as np
+
+ff = h5py.File("/mnt/LIMS/vol73/3100134/0001/m54006_151021_185942.trc.h5", "r")
+
+print ff.keys()
+# [u'ScanData', u'TraceData']
+
+print ff['TraceData'].keys()
+# [u'ActiveChipLooks', u'Codec', u'HPFrameSet', u'HoleChipLook', u'HoleNumber', u'HolePhase', u'HoleStatus', u'HoleType', u'HoleXY', u'HoleXYPlot', u'IFVarianceScale', u'OFBackgroundMean', u'OFBackgroundVariance', u'RVFrameSet', u'ReadVariance', u'Spectra', u'Traces', u'VFrameSet', u'Variance']
+
+dd = ff['TraceData']['Spectra']
+
+print dd.shape
+# (4, 207360, 2)
+
+print dd.dtype
+# dtype('float32')
